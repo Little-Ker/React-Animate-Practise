36774358076975073893ca8778998309aef715d5
Vivi <aa982273@gmail.com>
2023-08-29 00:02:55 +0800

    [新增] 刻大笨鐘

--- src/view/cssAnimateView/bigBen/BigBen.jsx
+++ src/view/cssAnimateView/bigBen/BigBen.jsx
@@ -6,15 +6,22 @@ import bigBen from '../../../assets/images/bigBen.svg'
     
 function BigBen() {
   const topRecAry = [...Array(10).keys()]
+  const bottomDecorateAry = [...Array(10).keys()]
+  const boxAry = [...Array(15).keys()]
 
   return (
     <div className={styles.bigBenView}>
-      <img className={styles.test} src={bigBen} alt="" />
+      {/* <img className={styles.test} src={bigBen} alt="" /> */}
       <label htmlFor="checkbox">
         <input type="checkbox" id="checkbox" />
         <div className={styles.scene}>
           {/* <h1 className={styles.title}>Click Change Sky!</h1> */}
           <div className={styles.sky}></div>
+          <div className={styles.bg}>
+            {boxAry.map((cur, index) => (
+              <div key={cur} className={styles[`box${index+1}`]}></div>
+            ))}
+          </div>
           <div className={styles.building}>
             <div className={styles.roof}>
               <div className={clsx(styles.cross, styles.cross1)}></div>
@@ -27,14 +34,14 @@ function BigBen() {
             <div className={styles.topWall1}></div>
             <div className={styles.topWall2}>
               <div className={clsx(styles.cross, styles.cross2)}></div>
+              <div className={clsx(styles.cross, styles.cross3)}></div>
               <div className={styles.rectList}>
-                {topRecAry.map((cur, index) => (
-                  <div key={cur} className={clsx(styles.rect, styles[`rect${index+1}`])}></div>
+                {topRecAry.map(cur => (
+                  <div key={cur} className={styles.rect}></div>
                 ))}
               </div>
-              <div className={clsx(styles.cross, styles.cross3)}></div>
             </div>
-            {/* <div className={styles.centerWall}>
+            <div className={styles.centerWall}>
               <div className={styles.clock}>
                 <div className={styles.centerDot}></div>
                 <div className={styles.longHand}></div>
@@ -42,8 +49,14 @@ function BigBen() {
               </div>
             </div>
             <div className={styles.bottomWall}>
-              
-            </div> */}
+              <div className={styles.bottomWall1}></div>
+              <div className={styles.bottomWall2}>
+                {bottomDecorateAry.map(cur => (
+                  <div key={cur} className={styles.decorate}></div>
+                ))}
+              </div>
+              <div className={styles.bottomWall3}></div>
+            </div>
           </div>
         </div>
       </label>
