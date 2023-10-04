ea6d21c257e38c8369bd849c0eb7b1cff574cf89
Vivi <vivichi@yile.com.tw>
2023-10-04 16:16:12 +0800

    [修改] 改用 swiper 製作選單

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {
+  useCallback, useEffect, useState
+} from 'react'
 import clsx from 'clsx'
 import styles from './navbar.module.sass'
 import {
   Link, useLocation  
 } from 'react-router-dom'
+
+import {
+  Swiper, SwiperSlide 
+} from 'swiper/react'
+import {
+  Navigation 
+} from 'swiper/modules'
+
+import 'swiper/css/navigation'
+import 'swiper/css'
+import 'swiper/css/pagination'
 
 const linkList = [{
   name: '偷吃蛋糕',
@@ -49,16 +62,67 @@ const linkList = [{
 export default function Navbar() {
   const location = useLocation()
 
+  console.log('location.pathname',location.pathname)
+
+  const [swiper, setSwiper] = useState(null)
+
+  useEffect(() => {
+    let findPathIndex = 0
+    
+    linkList.forEach((cur, index) => {
+      if (location.pathname === cur.to) {
+        findPathIndex = index
+      }
+    }, [])
+
+    if(swiper) swiper.slideTo(findPathIndex)
+  }, [linkList, location, swiper])
+
   return (
     <div className={styles.navbar}>
-      {linkList.map((link, index) => (
-        <Link
-          key={index}
-          to={link.to}
-          className={clsx(styles.link, location.pathname === `${link.to}` && styles.active)}>
-          {link.name}
-        </Link>
-      ))}
+      <Swiper
+        onSwiper={setSwiper}
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        navigation={{
+          prevEl: '#prevBtn',
+          nextEl: '#nextBtn',
+        }}
+        modules={[Navigation]}
+      >
+        {linkList.map((link, index) => (
+          <SwiperSlide key={index} className={styles.slide}>
+            <Link
+              to={link.to}
+              className={clsx(styles.link, location.pathname === `${link.to}` && styles.active)}>
+              {link.name}
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <a id={'prevBtn'} className={styles.swiperButton}>
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.5" fillRule="evenodd" clipRule="evenodd" d="M20.75 12C20.75 11.5858 20.4142 11.25 20 11.25H10.75V12.75H20C20.4142 12.75 20.75 12.4142 20.75 12Z" fill="#1C274C"></path> <path d="M10.75 18C10.75 18.3034 10.5673 18.5768 10.287 18.6929C10.0068 18.809 9.68417 18.7449 9.46967 18.5304L3.46967 12.5304C3.32902 12.3897 3.25 12.1989 3.25 12C3.25 11.8011 3.32902 11.6103 3.46967 11.4697L9.46967 5.46969C9.68417 5.25519 10.0068 5.19103 10.287 5.30711C10.5673 5.4232 10.75 5.69668 10.75 6.00002V18Z" fill="#1C274C"></path> </g></svg>
+      </a>
+      <a id={'nextBtn'} className={clsx(styles.swiperButton, styles.swiperNextBtn)}>
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.5" fillRule="evenodd" clipRule="evenodd" d="M20.75 12C20.75 11.5858 20.4142 11.25 20 11.25H10.75V12.75H20C20.4142 12.75 20.75 12.4142 20.75 12Z" fill="#1C274C"></path> <path d="M10.75 18C10.75 18.3034 10.5673 18.5768 10.287 18.6929C10.0068 18.809 9.68417 18.7449 9.46967 18.5304L3.46967 12.5304C3.32902 12.3897 3.25 12.1989 3.25 12C3.25 11.8011 3.32902 11.6103 3.46967 11.4697L9.46967 5.46969C9.68417 5.25519 10.0068 5.19103 10.287 5.30711C10.5673 5.4232 10.75 5.69668 10.75 6.00002V18Z" fill="#1C274C"></path> </g></svg>
+      </a>
     </div>
   )
 }
